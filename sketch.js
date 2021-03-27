8f3641185d8ec8959400749f8a45ec4a328c56f5
aditya00123 <aditgupta2010@gmail.com>
2021-03-27 16:43:04 +0530

    increasing canvas size

--- sketch.js
+++ sketch.js
@@ -11,30 +11,30 @@ function preload() {
 }
 
 function setup(){
-    var canvas = createCanvas(1200,400);
+    var canvas = createCanvas(1200,600);
     engine = Engine.create();
     world = engine.world;
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform = new Ground(150, 505, 300, 170);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+    box1 = new Box(700,520,70,70);
+    box2 = new Box(920,520,70,70);
+    pig1 = new Pig(810, 550);
+    log1 = new Log(810,460,300, PI/2);
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+    box3 = new Box(700,440,70,70);
+    box4 = new Box(920,440,70,70);
+    pig3 = new Pig(810, 420);
 
-    log3 =  new Log(810,180,300, PI/2);
+    log3 =  new Log(810,380,300, PI/2);
 
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+    box5 = new Box(810,360,70,70);
+    log4 = new Log(760,320,150, PI/7);
+    log5 = new Log(870,320,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(100,300);
 
 }
 
